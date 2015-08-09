717e9a064872f29b58f6103a259d65544a68cdb7
Adam Zerner <azerner3@gmail.com>
2015-08-08 20:09:43 -0400

    First iteration of karma providers tests

---
+++ client/tests/unit/auth.factory.spec.js
@@ -0,0 +1,8 @@
+// describe('Auth Factory', function() {
+//   var Auth;
+//
+//   beforeEach(module('mean-starter'));
+//   beforeEach(inject(function(_Auth_) {
+//     Auth = _Auth_;
+//   }));
+// });
---
+++ client/tests/unit/user.service.spec.js
@@ -0,0 +1,39 @@
+describe('User Service', function() {
+  var User, $httpBackend;
+
+  beforeEach(module('mean-starter'));
+  beforeEach(module('templates'));
+  beforeEach(inject(function(_User_, _$httpBackend_) {
+    User = _User_;
+    $httpBackend = _$httpBackend_;
+  }));
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('#list', function() {
+    $httpBackend.expectGET('/users').respond();
+    User.list();
+    $httpBackend.flush();
+  });
+
+  it('#get', function() {
+    $httpBackend.expectGET('/users/1').respond();
+    User.get(1);
+    $httpBackend.flush();
+  });
+
+  it('#update', function() {
+    var user = { username: 'adamzerner' };
+    $httpBackend.expectPUT('/users/1', user).respond();
+    User.update(1, user);
+    $httpBackend.flush();
+  });
+
+  it('#delete', function() {
+    $httpBackend.expectDELETE('/users/1').respond();
+    User.delete(1);
+    $httpBackend.flush();
+  });
+});
