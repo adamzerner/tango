c591f1c1bac1c9cbbebd52c783e46e42b36511f9
Adam Zerner <azerner3@gmail.com>
2015-09-04 18:42:56 -0400

    autosize works

--- client/src/run/run.js
+++ client/src/run/run.js
@@ -1,13 +1,22 @@
 angular
   .module('tango')
   .run(run)
+  .directive('textarea', textarea)
 ;
 
 function run(Auth, $rootScope) {
   $rootScope.user = {};
   Auth.requestCurrentUser();
+}
 
-  angular.element(document).ready(function() {
-    autosize(document.getElementsByTagName('textarea'));
-  });
+// makes it so that textareas automatically expand vertically as new lines are added
+function textarea($timeout) {
+  return {
+    restrict: 'E',
+    link: function(scope, iElement) {
+      $timeout(function() {
+        autosize(iElement[0]);
+      }, 0);
+    }
+  };
 }
