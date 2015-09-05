0ad1ce364074504141d5eed9bb25563797605325
Adam Zerner <azerner3@gmail.com>
2015-09-05 14:52:56 -0400

    Start off with one element with focus

--- client/src/states/tangos/directive/statement.directive.js
+++ client/src/states/tangos/directive/statement.directive.js
@@ -12,7 +12,13 @@ function statement(RecursionHelper) {
     },
     templateUrl: '/states/tangos/directive/statement.directive.html',
     compile: function(tElement) {
-      return RecursionHelper.compile(tElement);
+      return RecursionHelper.compile(tElement, function(scope, iElement, iAttrs) {
+        // start off with one statement with focus
+        if (scope.statement.focus) {
+          scope.focus = true;
+          iElement.find('textarea')[0].focus();
+        }
+      });
     }
   };
 }
--- client/src/states/tangos/new/tango.new.controller.js
+++ client/src/states/tangos/new/tango.new.controller.js
@@ -5,20 +5,25 @@ angular
 
 function NewTangoController($scope) {
   var vm = this;
+  // vm.statements = [{
+  //   text: '1',
+  //   children: []
+  // }, {
+  //   text: '2',
+  //   children: [{
+  //     text: '2.1',
+  //     children: []
+  //   }, {
+  //     text: '2.2',
+  //     children: [{
+  //       text: '2.2.1',
+  //       children: []
+  //     }]
+  //   }]
+  // }];
   vm.statements = [{
-    text: '1',
-    children: []
-  }, {
-    text: '2',
-    children: [{
-      text: '2.1',
-      children: []
-    }, {
-      text: '2.2',
-      children: [{
-        text: '2.2.1',
-        children: []
-      }]
-    }]
-  }];
+    text: '',
+    children: [],
+    focus: true // start off with a statement with focus
+  }]
 }
