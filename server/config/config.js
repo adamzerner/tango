a002ad09504e85648f23a9b801997d27b7f25812
Adam Zerner <azerner3@gmail.com>
2015-10-04 22:38:15 -0400

    Using variables instead of strings

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,1 @@
-module.exports = process.env.NODE_ENV === 'production' ? require('./config.prod.json') : require('./config.dev.json');
+module.exports = process.env.NODE_ENV === 'production' ? require('./config.prod.js') : require('./config.dev.js');
